6554bbbd5d741ad105547752e0800c897c8cab60
只是蘋果JustApple <114324056+JustappleJust@users.noreply.github.com>
2026-07-19 20:30:24 +0800

    backup

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,78 @@
 /*
 @jnode/dns
-esm
+esm export
 
 Simple DNS server package for Nodejs.
 
-by JNode Dev Team / JustApple
+by Codex (ESM exporting)
 */
 
+import DnsPacket from './packet.js';
+import server from './server.js';
+import DnsClient from './client.js';
+import routers from './routers.js';
+import handlers from './handlers.js';
+
+const {
+    defaultHandler,
+    errorHandler,
+    createServer,
+    DnsServer
+} = server;
+
+const {
+    LabelRouter,
+    TypeRouter,
+    FunctionRouter,
+    LabelArgRouter,
+    SetRegisteryRouter,
+    routerConstructors
+} = routers;
+
+const {
+    ErrorHandler,
+    PacketHandler,
+    RecordHandler,
+    DnsClientHandler,
+    handlerConstructors
+} = handlers;
+
+export {
+    DnsPacket,
+    defaultHandler,
+    errorHandler,
+    createServer,
+    DnsServer,
+    DnsClient,
+    LabelRouter,
+    TypeRouter,
+    FunctionRouter,
+    LabelArgRouter,
+    SetRegisteryRouter,
+    routerConstructors,
+    ErrorHandler,
+    PacketHandler,
+    RecordHandler,
+    DnsClientHandler,
+    handlerConstructors
+};
+
+export default {
+    DnsPacket,
+    defaultHandler,
+    errorHandler,
+    createServer,
+    DnsServer,
+    DnsClient,
+    LabelRouter,
+    TypeRouter,
+    FunctionRouter,
+    LabelArgRouter,
+    SetRegisteryRouter,
+    routerConstructors,
+    ErrorHandler,
+    PacketHandler,
+    RecordHandler,
+    DnsClientHandler,
+    handlerConstructors
+};
